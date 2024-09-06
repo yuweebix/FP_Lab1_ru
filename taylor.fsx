eeeb718bc606746414eabe51c45649a937584f4a
yuweebix <yuweebix@gmail.com>
2024-09-06 15:57:17 +0300

    task 1

--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,52 @@
-// Print a table of a given function f, computed by taylor series
+let rec FOR init cond iter func args =
+    match cond init with
+    | true -> FOR (iter init) cond iter func (func init args)
+    | false -> args
 
-// function to compute
-let f = sin
+let pown x n =
+    FOR
+        0
+        (fun i -> i < n)
+        (fun i -> i + 1)
+        (fun _ -> fun res -> res * x)
+        1.0
 
-let a = 0.0
-let b = 1.0
-let n = 10
+let f x = x / (9.0 + pown x 2)
+let term x n = (pown -1.0 n) * (pown x (2*n+1)) / (pown 9.0 (n+1))
 
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
+let naiven x n =
+    FOR
+        (0, x)
+        (fun (i, _) -> i <= n)
+        (fun (i, x) -> (i+1, x))
+        (fun (i, x) -> fun res -> res + (term x i))
+        0.0
 
+let naive x eps =
+    FOR
+        (0, term x 0)
+        (fun (i, xi) -> abs ((f x) - xi) > eps)
+        (fun (i, xi) -> (i+1, naiven x i))
+        (fun (i, xi) -> fun (res, num) -> (xi, i+1))
+        (0.0, 0)
+    
+let smart x eps =
+    FOR
+        (0, 0.0)
+        (fun (i, xi) -> abs ((f x) - xi) > eps && i < 10)
+        (fun (i, xi) -> (i+1, xi + (term x i)))
+        (fun (i, xi) -> fun (res, num) -> (xi, i+1))
+        (0.0, 0)
 
-// Define a function to do the same in a more efficient way
-let taylor = f
+let printTable a b n eps =
+    let step = (b - a) / float (n - 1)
+    printfn "| x     | Builtin     | Smart Taylor | # terms | Dumb Taylor | # terms |"
+    printfn "|-------|-------------|--------------|---------|-------------|---------|"
+    for i in 0 .. n-1 do
+        let x = a + step * float i
+        let builtin = f x
+        let (smartResult, smartTerms) = smart x eps
+        let (dumbResult, dumbTerms) = naive x eps
+        printfn "| %5.1f | %11.5f | %12.5f | %7d | %11.5f | %7d |" x builtin smartResult smartTerms dumbResult dumbTerms
 
-let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
-
-main
-
+printTable -1.0 1.0 21 1e-5
